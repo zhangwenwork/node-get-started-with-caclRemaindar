2fe7b37852af78c35708d8444b64a886f8fb022b
Wen <zhangwen.working@gmail.com>
2019-09-23 13:47:26 +0800

    fix the test

--- main/main.js
+++ main/main.js
@@ -1,9 +1,26 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar(divisor,dividend) {
+	return divisor%dividend;
+};
 
-function caclSum() {};
+function caclSum(array) {
+	let caclSum = 0;
+	for (var i = 0; i < array.length; i++) {
+		caclSum = caclSum + array[i];
+	}
+	return caclSum;
+};
 
-function caclSumInConditon() {};
+function caclSumInConditon(array, indicatedNum) {
+	let caclSum = 0;
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] < indicatedNum) {
+			caclSum = caclSum + array[i];
+		}
+		
+	}
+	return caclSum;
+};
 
 module.exports = {
     caclRemaindar,
